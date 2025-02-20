48bfa3dfb3a59517690c0d7cc0af51dfb68ca974
Cangregito <Jassiel.rr1502@gmail.com>
2025-02-20 13:17:36 -0700

    Add initial page components and routing for Home, Ayuda, Custumer, and Servicios

--- src/app/custumer/custumer.page.ts
+++ src/app/custumer/custumer.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common'; 
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -7,13 +8,14 @@ import { RouterModule } from '@angular/router';
   templateUrl: './custumer.page.html',
   styleUrls: ['./custumer.page.scss'],
   standalone: true,
-  imports: [IonicModule, RouterModule]
+  imports: [IonicModule, RouterModule, CommonModule] 
 })
 export class CustumerPage implements OnInit {
 
+  items: string[] = ['Cliente 1', 'Cliente 2', 'Cliente 3', 'Cliente 4'];
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
